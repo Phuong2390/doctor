f342319f273f7d2cd7eb5ce22926ecb386da03d1
Phuong2390 <vanphuongk38cntt@gmail.com>
2018-04-04 13:57:29 +0700

    edit searcher-keyword

--- src/search-services/searcher.js
+++ src/search-services/searcher.js
@@ -5,7 +5,11 @@ class Searcher {
 	
 	search(condition) {
 		let sqlQuery = this.connection
-			.select('hospitals.name').from('hospitals');
+			.select('hospitals.name','hospitals.location_id', 'locations.address', 'locations.lat', 'locations.long')
+			.from('hospitals')
+			.innerJoin('locations', function () {
+				this.on('hospitals.location_id', '=', 'locations.id')
+			});
 		condition.describe(sqlQuery);
 		return sqlQuery;
 	}
